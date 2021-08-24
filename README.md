0ab234c57bd8b458eb6cba6952cfc4ea75c908df
Michal Sojka <michal.sojka@cvut.cz>
2021-08-24 10:55:08 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # EmacsVterm
+
+## Usage
+
+Add the following to `~/.julia/config/startup.jl`:
+
+```julia
+atreplinit() do repl
+    @eval using EmacsVterm
+end
+```
--- src/EmacsVterm.jl
+++ src/EmacsVterm.jl
@@ -1,5 +1,31 @@
 module EmacsVterm
 
-# Write your package code here.
+import REPL
+
+const user = get(ENV, "USER", "")
+vterm_cmd(str) = "\e]$str\e\\"
+prompt_suffix() = vterm_cmd("51;A$(user)@$(gethostname()):$(pwd())")
+eval_elisp(elisp) = vterm_cmd("51;E$(elisp)")
+
+function __init__()
+    if !(isinteractive() && isdefined(Base, :active_repl))
+        return
+    end
+    begin
+        if get(ENV, "INSIDE_EMACS", "") == "vterm"
+            @info "Emacs vterm detected"
+            repl = Base.active_repl
+
+            if !isdefined(repl,:interface)
+                repl.interface = REPL.setup_interface(repl)
+            end
+
+            suffix = repl.interface.modes[1].prompt_suffix
+            repl.interface.modes[1].prompt_suffix = function ()
+                ((isa(suffix,Function) ? suffix() : suffix) * prompt_suffix())
+            end
+        end
+    end
+end
 
 end
